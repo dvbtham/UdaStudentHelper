3290d5621c3b4c887216d91232b158945117c881
tham davies <thamdv96@gmail.com>
2018-02-02 10:52:22 +0700

    added project files.

---
+++ popup.js
@@ -0,0 +1,20 @@
+$(function () {  
+    $("#contactForm").submit(function(){
+        var form = $(this);
+        var id = form.attr('id');
+        var data = form.serialize();
+        var actionsTemp = data.replace("=", "");
+        actionsTemp = actionsTemp.replace("=", "");
+        var actions = actionsTemp.split("&");
+               
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: actions });
+        });
+      });
+
+      $("#reset").on('click', function(){
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "reset" });
+        });
+      })
+});
---
+++ uda.js
@@ -0,0 +1,79 @@
+var udaHelper = {
+    dataTable: {},
+    tinchihoanthanh: "",
+    diemtrungbinh:  "",
+    options: {
+        'lengthMenu': [[10, 20, 30, 50, -1], [10, 20, 30, 50, "Tất cả"]],
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": true,
+        "language": {
+            sProcessing: "Đang xử lý...",
+            sLengthMenu: "Xem _MENU_ mục",
+            sZeroRecords: "Không tìm thấy dòng nào phù hợp",
+            sInfo: "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ mục",
+            sInfoEmpty: "Đang xem 0 đến 0 trong tổng số 0 mục",
+            sInfoFiltered: "(được lọc từ _MAX_ mục)",
+            sInfoPostFix: "",
+            sSearch: "Tìm kiếm:",
+            sUrl: "",
+            oPaginate: {
+                sFirst: "Đầu",
+                sPrevious: "Trước",
+                sNext: "Tiếp",
+                sLast: "Cuối"
+            }
+        }
+    }
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+    if (request.action[0] == "smartTable") {
+        smartTable.init();
+        return;
+    }
+    if (request.action == "reset") {
+        smartTable.destroy();
+        $('table:first > tbody').append(udaHelper.tinchihoanthanh + udaHelper.diemtrungbinh);
+        return;
+    }
+});
+
+chrome.runtime.sendMessage({ action: "show" });
+
+var smartTable = {
+    destroy: function () {
+        if(!udaHelper.dataTable) return;
+
+        $.each(udaHelper.dataTable, function(){
+            $(this).dataTable().fnDestroy();
+        });
+        $("#top-statistic").remove();
+        $("#main-header").unstick();
+    },
+    init: function () {
+        var soTinChi = $('table:first > tbody > tr:nth-last-child(2) > td:nth-last-child(2)').text();
+        var tongDiem = $('table:first > tbody > tr:last > td:nth-last-child(2)').text();
+
+        udaHelper.tinchihoanthanh = "<tr>" + $('table:first > tbody > tr:nth-last-child(2)').html() + "</tr>";
+        udaHelper.diemtrungbinh = "<tr>" + $('table:first > tbody > tr:last').html() + "</tr>";
+        
+        $('table:first > tbody > tr').slice(-2).remove();
+
+        $("#main-header").sticky({ topSpacing: 0 });
+
+        $(".header-top > .row > div:last").append("<p id='top-statistic' style='color: #fff; font-size: 16px;padding-top: 8px;'>Tổng điểm: " + tongDiem + " - Hoàn thành: " + soTinChi + " (tín chỉ).</p>");
+
+        $('table').dataTable(udaHelper.options);
+        udaHelper.dataTable = $.fn.dataTable.fnTables(true);
+        $('#main-header').on('sticky-start', function () {
+            $('#main-header').addClass("animated fadeInDown z-index");
+        });
+        $('#main-header').on('sticky-end', function () {
+            $('#main-header').removeClass("animated fadeInDown z-index");
+        });
+    }
+}
