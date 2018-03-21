fbb23bec71cf66a579a76a74d59cbdc57b7980aa
tham davies <thamdv96@gmail.com>
2018-03-21 21:43:25 +0700

    remove unnecessary files.

--- popup.js
+++ popup.js
@@ -16,5 +16,12 @@ $(function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: "reset" });
         });
-      })
+      });
+
+      $("#sendMail").on('click', function(e){
+          e.preventDefault();
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "sendMail" });
+        });
+      });
 });
